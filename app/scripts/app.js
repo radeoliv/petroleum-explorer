0d361058fd566e4e01bf60077c019dcac5523098
Sean Goresht <seangores@gmail.com>
2014-01-21 11:36:56 -0700

    Initial commit Generate project from yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,9 @@
+var Seng515PetroleumExplorer = window.Seng515PetroleumExplorer = Ember.Application.create();
+
+/* Order and include as you please. */
+require('scripts/controllers/*');
+require('scripts/store');
+require('scripts/models/*');
+require('scripts/routes/*');
+require('scripts/views/*');
+require('scripts/router');
---
+++ app/scripts/store.js
@@ -0,0 +1,1 @@
+Seng515PetroleumExplorer.ApplicationAdapter = DS.FixtureAdapter;
