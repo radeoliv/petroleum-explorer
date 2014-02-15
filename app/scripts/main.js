adfad3c09b92c870e353b529f5fd17744f37766a
Adesh Banvait <adesh911@gmail.com>
2014-02-14 18:59:56 -0700

    Added config.rb for SCSS compliation, Added map-controller module Fixed errors with maps not showing on load Remove buttons and update sidebar according to client's request.

---
+++ app/scripts/main.js
@@ -0,0 +1,36 @@
+/*--------------------------------------------------------------------------------
+ Author: admin
+ www: http://seangoresht.com/
+ github: https://github.com/srsgores
+
+ twitter: http://twitter.com/S.Goresht
+
+ seng-515-petroleum-explorer-rodrigo
+
+ =============================================================================
+ Filename:main.js
+ =============================================================================
+ //TODO: file description
+ -------------------------------------------------------------------------------*/
+jQuery(document).ready(function ($) {
+	/* =============================================================================
+	 Variables
+	 ========================================================================== */
+	var mapToolbar = $(".mapToolbar"),
+		toolbarToggle = mapToolbar.find(".show-toolbar"),
+		mapToolbarIcon = toolbarToggle.find("i"),
+		mapToolbarContent = mapToolbar.find(".toolbar-content");
+
+	function initMapToggle() {
+		hideMapContent(mapToolbarContent);
+		toolbarToggle.on("click", function (e) {
+			mapToolbarContent.toggle("slow");
+			// toggle icon
+			mapToolbarIcon.toggleClass("icon-arrow-left icon-arrow-right");
+		});
+	}
+	function hideMapContent(mapContent) {
+		mapContent.hide();
+	}
+	initMapToggle();
+});
