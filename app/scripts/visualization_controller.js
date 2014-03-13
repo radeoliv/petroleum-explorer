cad88ea7332f31fa5636fa54f064b22cdc9962d9
sceastwo@ucalgary.ca <sceastwo@ucalgary.ca>
2014-03-13 08:49:37 -0600

    Oil_well_filter implementation worked on.

--- app/scripts/visualization_controller.js
+++ app/scripts/visualization_controller.js
@@ -24,17 +24,19 @@
 
 			this.visualization_method = ""; //visualization method is a string from {"histogram", "pie-chart"} that denotes the method of visualization that is to be used.
 			this.start = 0; //For bar histograms, start will indicate the beginning point of the vertical axis. This will better emphasize absolute differences in quantity.
+			this.end = 0; //For bar histograms, end will indicate the ending point of the vertical axis.
 			this.category_widths = 0.0; //For continuous data, category_widths denotes the width of each interval within which all quantities are given the same classification.
 
-		};
+		}
 
 		Oil_well_filter.prototype.get_visualization_method = function () {
 			this.categories = [];
 			this.category_counts = [];
 
+			var N = this.oil_data.length;
+
 			if (this.discrete_flag) {
 				//If the data is discrete, determine the categories and frequencies.
-				var N = this.oil_data.length;
 				for (var i = 0;
 					i < N;
 					i++) {
@@ -57,41 +59,62 @@
 						this.category_counts.push(1);
 					}
 				}
-				//Find the maximum and minimum counts. If the data set is empty, these quantities default to 0.
-				var max_count = 0;
-				var min_count = 0;
-				if (this.category_counts.length > 0) {
-					max_count = this.category_counts[0];
-					min_count = this.category_counts[0];
-					for (var i = 1;
-						i < this.category_counts.length;
-						i++) {
-						if (this.category_counts[i] > max_count) {
-							max_count = this.category_counts[i];
+			}
+			else {
+				//If the data is continuous, we need to first find a useful category width:
+				//We will find the minimum and maximum data points: If the data set is empty, these quantities default to 0.
+				var data_min = 0;
+				var data_max = 0;
+				if (N > 0) {
+					data_min = this.oil_data[0];
+					data_max = this.oil_data[0];
+					for (var i = 1; i < N; i++) {
+						if (this.oil_data[i] > data_max) {
+							data_max = this.oil_data[i];
 						}
-						if (this.category_counts[i] < min_count) {
-							min_count = this.category_counts[i];
+						if (this.oil_data[i] < data_min) {
+							data_min = this.oil_data[i];
 						}
 					}
 				}
-				var threshold = 1.0 //The the threshold of (max_count-min_count)/min_count above which the pie-chart will be used. Large values of (max_count-min_count)/min_count indicate great relative differences between the counts which the pie-chart a good choice.
-				if ((max_count - min_count) / min_count >= threshold) {
-					//Use the pie-chart
-					this.visualization_method = "pie-chart";
-					return this.visualization_method;
+			}
+			//Find the maximum and minimum counts. If the data set is empty, these quantities default to 0.
+			var max_count = 0;
+			var min_count = 0;
+			if (this.category_counts.length > 0) {
+				max_count = this.category_counts[0];
+				min_count = this.category_counts[0];
+				for (var i = 1;
+					i < this.category_counts.length;
+					i++) {
+					if (this.category_counts[i] > max_count) {
+						max_count = this.category_counts[i];
+					}
+					if (this.category_counts[i] < min_count) {
+						min_count = this.category_counts[i];
+					}
 				}
-				else {
-					//Use the histogram
-					this.visualization_method = "histogram";
-					//Find a useful starting point
+			}
+			var threshold = 1.0 //The the threshold of (max_count-min_count)/min_count above which the pie-chart will be used. Large values of (max_count-min_count)/min_count indicate great relative differences between the counts which makes the pie-chart a good choice.
 
-				}
+			if (min_count > 0 ? ((max_count - min_count) / min_count >= threshold) : true) {
+				//Use the pie-chart
+				this.visualization_method = "pie-chart";
 			}
 			else {
-				//If the data is continuous:
+				//Use the histogram
+				this.visualization_method = "histogram";
+				//Find a useful starting point
+				threshold = 0.5; //The the threshold of (max_count-min_count)/min_count above which the starting point of vertical axis will be left at 0.
+				if ((max_count - min_count) / min_count >= threshold) {
+					this.start = 0;
+				}
+				else {
+					this.start = min_count - Math.floor((max_count - min_count) / threshold);
+				}
+				//Find a useful ending point
+				this.end = max_count + 1;
 			}
-
-			this.visualization_method = "histogram";
 
 			return this.visualization_method;
 		};
