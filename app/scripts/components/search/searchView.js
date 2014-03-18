bcb67fcb500689267b00f57b9cfe4c92604f6177
Adesh Banvait <adesh911@gmail.com>
2014-03-17 20:01:48 -0600

    updated sidebar with accordion to select search option. Working uwi/fields/company search. Status search needs to be fixed using proper event callers. Fixed search by meridian. Refactored code from searchView to Search-Controller for better design and testing purposes.

--- app/scripts/components/search/searchView.js
+++ app/scripts/components/search/searchView.js
@@ -19,14 +19,20 @@ SearchView = function (searchController) {
 	this.searchController = searchController
 };
 
-/*
- * Static error messages
- */
-var lsdErrorMsg = "<b>LSD</b> value must be within 01 and 16.",
-	sectionErrorMsg = "<b>SEC</b> value must be within 01 and 36.",
-	townshipErrorMsg = "<b>TWP</b> value must be within 001 and 126.",
-	rangeErrorMsg = "<b>RNG</b> value must be within 01 and 34.",
-	meridianErrorMsg = "<b>MER</b> value must be within 1 and 6.";
+var optionAccordion;	//global var for handling accordion option for search select
+
+(function () {
+	$(function () {
+		var active;
+		active = function () {
+			return $( "#accordion" ).on("click", function () {
+				optionAccordion = $( "#accordion" ).accordion( "option", "active" );
+			});
+		}
+		active();
+	});
+}).call(this);
+
 
 /**
  * listen to key down events
@@ -43,11 +49,13 @@ SearchView.prototype.listenKeyboard = function ($searchInputSelector, $searchInp
 		townshipSearchInput = $searchInputForm.find("input[name='township']"),
 		rangeSearchInput = $searchInputForm.find("input[name='range']"),
 		meridianSearchInput = $searchInputForm.find("input[name='meridian']"),
-		companySearchInput = $searchInputForm.find("input[name='company']");
+		companySearchInput = $searchInputForm.find("input[name='company']"),
+		statusSearchInput = $searchInputForm.find("select[name='status']");
 
 	var self = this;
 
 	$searchInputSelector.on("keyup", function (e) { //retrieving the value in the input and saving it as a string variable.  Whenever a search input has typed in it, performs the search
+
 		forceNumeric([lsdSearchInput, sectionSearchInput, townshipSearchInput, rangeSearchInput, meridianSearchInput]);
 
 		var uwiQuery=uwiSearchInput[0].value,
@@ -56,10 +64,50 @@ SearchView.prototype.listenKeyboard = function ($searchInputSelector, $searchInp
 			townshipQuery=townshipSearchInput[0].value,
 			rangeQuery=rangeSearchInput[0].value,
 			meridianQuery=meridianSearchInput[0].value,
-			companyQuery=companySearchInput[0].value;
+			companyQuery=companySearchInput[0].value,
+			statusQuery=statusSearchInput[0].value;
 
-		if(checkUWIValueInputs([lsdQuery, sectionQuery, townshipQuery, rangeQuery, meridianQuery])) {
-			var results = self.searchController.findResults(lsdQuery, sectionQuery, townshipQuery, rangeQuery, meridianQuery, companyQuery); // return json object back
+			console.log(statusQuery);
+
+		var results;
+		var error = false;
+
+		switch(optionAccordion) {
+			// Search by the entire UWI
+			case 0:
+				if(checkUWIInput(uwiQuery))
+					results = self.searchController.findResultsUWI(uwiQuery);
+				else
+					error = true;
+				break;
+			// Search by the UWI values
+			case 1:
+				if(checkUWIValueInputs([lsdQuery, sectionQuery, townshipQuery, rangeQuery, meridianQuery]))
+					results = self.searchController.findResultsUWIValues(lsdQuery, sectionQuery, townshipQuery, rangeQuery, meridianQuery);
+				else
+					error = true;
+				break;
+			// Search by the company name
+			case 2:
+				if(checkCompanyInput(companyQuery))
+					results = self.searchController.findResultsCompany(companyQuery);
+				else
+					error = true;
+				break;
+			// Search by the status
+			case 3:
+				// What now? hahahaha
+				break;
+			default:
+				console.log("Accordeon error!");
+				break;
+		}
+
+		if(!error) {
+			// Cleaning the results area
+			if($('#results-table') != undefined) {
+				$('#results-table').remove();
+			}
 
 			var $resultsArea = $('<div id="results-table" class="handsontable"></div>').appendTo($searchInputForm); //append the results container if javascript enabled
 
@@ -101,109 +149,12 @@ function forceNumeric(fields) {
 }
 
 function clearFields() {
-	// Clear all fields using field.val
-}
-
-function checkUWIValueInputs(fieldValues) {
-	// Checking UWI inputs for appropriate length and range of values
-	// Values in fieldValues are in-order (lsd, section, township, range, meridian)
-
-	// Township, Range and Meridian are mandatory fields.
-	// If any one of these three fields is filled properly, the search should be executed.
-
-	$( ".error-msg" ).remove();
-	var lsd = fieldValues[0],
-		section = fieldValues[1],
-		township = fieldValues[2],
-		range = fieldValues[3],
-		meridian = fieldValues[4];
-
-	var divToAppend = '.search-form';
-
-	var validity = true;
-	var mandatoryField = false;
-
-	// TODO: Create a function to append error messages modularly.
-
-	/*
-	 * Township
-	 */
-	if(township.length === 3) {
-		if(!(township >= 1 && township <= 126)) {
-			console.log("range shits!");
-			$(createErrorMessage(townshipErrorMsg)).appendTo(divToAppend);
-			validity = false;
-		} else {
-			mandatoryField = true;
-		}
-	} else if(township.length > 0) {
-		console.log("length shits!");
-		validity = false;
-	}
-
-	/*
-	 * Range
-	 */
-	if(range.length === 2) {
-		if(!(range >= 1 && range <= 34)) {
-			console.log("range shits!");
-			$(createErrorMessage(rangeErrorMsg)).appendTo(divToAppend);
-			validity = false;
-		} else {
-			mandatoryField = true;
-		}
-	} else if(range.length > 0) {
-		console.log("length shits!");
-		validity = false;
-	}
-
-	/*
-	 * Meridian
-	 */
-	if(meridian.length === 1) {
-		if(!(meridian >= 1 && meridian <= 6)) {
-			console.log("range shits!");
-			$(createErrorMessage(meridianErrorMsg)).appendTo(divToAppend);
-			validity = false;
-		} else {
-			mandatoryField = true;
-		}
-	} else if(meridian.length > 0) {
-		console.log("length shits!");
-		validity = false;
-	}
-
-	/*
-	 * LSD
-	 */
-	if(lsd.length === 2) {
-		if(!(lsd >= 1 && lsd <= 16)) {
-			console.log("range shits!");
-			$(createErrorMessage(lsdErrorMsg)).appendTo(divToAppend);
-			validity = false;
-		}
-	} else if(lsd.length > 0) {
-		console.log("length shits!");
-		validity = false;
-	}
-
-	/*
-	 * Section
-	 */
-	if(section.length === 2) {
-		if(!(section >= 1 && section <= 36)) {
-			console.log("range shits!");
-			$(createErrorMessage(sectionErrorMsg)).appendTo(divToAppend);
-			validity = false;
-		}
-	} else if(section.length > 0) {
-		console.log("length shits!");
-		validity = false;
-	}
-
-	return validity && mandatoryField;
-}
-
-function createErrorMessage(message) {
-	return '<label class = "error-msg">'+message+'</br></label>';
+	$searchInputForm.find("input[name='uwi']").val(''),
+	$searchInputForm.find("input[name='lsd']").val(''),
+	$searchInputForm.find("input[name='section']").val(''),
+	$searchInputForm.find("input[name='township']").val(''),
+	$searchInputForm.find("input[name='range']").val(''),
+	$searchInputForm.find("input[name='meridian']").val(''),
+	$searchInputForm.find("input[name='company']").val(''),
+	$searchInputForm.find("select[name='status']").val('');
 }
