eaa891426546bd472948329fa36641bf7edaabd7
sceastwo@ucalgary.ca <sceastwo@ucalgary.ca>
2014-03-19 18:54:58 -0600

    Oil_well_filter.get_visualization_method completed (Shawn and Mandy)

--- app/scripts/visualization_controller.js
+++ app/scripts/visualization_controller.js
@@ -19,6 +19,7 @@
 			this.oil_data = oil_data; //oil_data is an array that stores the data that needs to be visualized. The data set should never be empty.
 			this.discrete_flag = discrete_flag; //discrete_flag is a boolean variable that is true if the data consists of discrete quantities and is false if otherwise.
 
+			this.numofCategories = 0; //The number of categories in the histogram/pie-chart.
 			this.categories = []; //An array that holds the categories of the histogram/pie-chart.
 			this.category_counts = []; //An array that holds the number of occurrences of each category.
 
@@ -26,28 +27,35 @@
 			this.start = 0; //For bar histograms, start will indicate the beginning point of the vertical axis. This will better emphasize absolute differences in quantity.
 			this.end = 0; //For bar histograms, end will indicate the ending point of the vertical axis.
 
-			this.beginning = 0.0; //For continuous data, beginning denotes the value where the categories should begin.
-			this.end = 0.0; //For continuous data, end denotes the value where the categories should end.
+			this.startpoint = 0.0; //For continuous data, beginning denotes the value where the categories should begin.
+			this.endpoint = 0.0; //For continuous data, end denotes the value where the categories should end.
 			this.category_widths = 0.0; //For continuous data, category_widths denotes the width of each interval within which all quantities are given the same classification.
 
 		}
 
 		Oil_well_filter.prototype.get_visualization_method = function () {
+			//Initialization:
+			this.numofCategories = 0;
 			this.categories = [];
 			this.category_counts = [];
 
 			var N = this.oil_data.length;
+
+			if (N == 0) {
+				return "Data set cannot be empty."; //Return an error if the data set is empty.
+			}
 
 			if (this.discrete_flag) {
 				//If the data is discrete, determine the categories and frequencies.
 				for (var i = 0;
 					i < N;
 					i++) {
+					var element = this.oil_data[i];
 					//Determine if element already exists in "categories".
 					var category_index = -1;
-					var j = 0;
-					while (category_index == -1 && j < this.categories.length) {
-						if (this.oil_data[i] == this.categories[j]) {
+					var j = 0; //Iterates through the already determined categories.
+					while (category_index == -1 && j < this.numofCategories) {
+						if (element == this.categories[j]) {
 							category_index = j;
 						}
 						j++;
@@ -58,6 +66,7 @@
 					}
 					else {
 						//Else add a new category.
+						this.numofCategories++;
 						this.categories.push(this.oil_data[i]);
 						this.category_counts.push(1);
 					}
@@ -65,12 +74,14 @@
 			}
 			else {
 				//If the data is continuous, we need to first find a useful category width:
-				//We will find the minimum and maximum data points: If the data set is empty, these quantities default to 0.
+				//We will find the minimum and maximum data points:
 				var data_min = 0;
 				var data_max = 0;
 				data_min = this.oil_data[0];
 				data_max = this.oil_data[0];
-				for (var i = 1; i < N; i++) {
+				for (var i = 1;
+					i < N;
+					i++) {
 					if (this.oil_data[i] > data_max) {
 						data_max = this.oil_data[i];
 					}
@@ -80,12 +91,29 @@
 				}
 
 				var data_interval = data_max - data_min;
-				//The category width will be the average distance between the data points sorted in increasing order. This defaults to 0.0 for the empty data set.
-				this.category_widths = data_interval/N;
-				//Now create the categories and classify the data points accordingly.
+				//The number of categories will be the square root of the number of data points:
+				this.numofCategories = Math.sqrt(N);
+				this.category_widths = data_interval / (this.numofCategories - 1);
+				//Now create the categories:
+				this.startpoint = data_min - this.category_widths / 2; //The lowest category is centered on the lowest data point.
+				this.endpoint = data_max + this.category_widths / 2; //The highest category is centered on the highest data point.
+				//this.categories = new Array(this.numofCategories);
+				for (var i = 0; i < this.numofCategories; i++)
+				{
+					var curr_lowerbound = this.startpoint + i*this.category_widths;
+					var curr_upperbound = this.startpoint + (i+1)*this.category_widths;
+					this.categories[i] = "[" + curr_lowerbound + "," + curr_upperbound + ")";
+				}
+
+				//We now classify the data.
+				for (var i = 0; i < N; i++) {
+					var element = this.oil_data[i];
+					var category_index = Math.floor((element - this.startpoint)/this.category_widths);
+					this.category_counts[category_index]++;
+				}
 			}
 
-			//Find the maximum and minimum counts. If the data set is empty, these quantities default to 0.
+			//Find the maximum and minimum counts.
 			max_count = this.category_counts[0];
 			min_count = this.category_counts[0];
 			for (var i = 1;
@@ -100,7 +128,7 @@
 			}
 
 			var threshold = 1.0 //The the threshold of (max_count-min_count)/min_count above which the pie-chart will be used. Large values of (max_count-min_count)/min_count indicate great relative differences between the counts which makes the pie-chart a good choice.
-			if (min_count > 0 ? ((max_count - min_count) / min_count >= threshold) : true) {
+			if ((max_count - min_count) / min_count >= threshold) {
 				//Use the pie-chart
 				this.visualization_method = "pie-chart";
 			}
@@ -108,8 +136,9 @@
 				//Use the histogram
 				this.visualization_method = "histogram";
 				//Find a useful starting point
-				threshold = 0.5; //The the threshold of (max_count-min_count)/min_count above which the starting point of vertical axis will be left at 0.
+				threshold = 0.5; //The threshold of (max_count-min_count)/min_count above which the starting point of vertical axis will be left at 0.
 				if ((max_count - min_count) / min_count >= threshold) {
+					//Relative differences can be seen without truncating the vertical axis from below.
 					this.start = 0;
 				}
 				else {
