5b97d6ac65edf826ae9505a397fe52171e4722d3
Rodrigo Silva <radeoliv@ucalgary.ca>
2014-04-08 19:41:50 -0600

    Code for the oil production chart is now separated and in a new file (info-graph.js). Added the Nivo-Lightbox to hold the chart.

--- app/scripts/components/visualizationController/info-graph.js
+++ app/scripts/components/visualizationController/info-graph.js
@@ -1,6 +1,6 @@
  /*--------------------------------------------------------------------------------
 	Author: Rodrigo Silva
-	github: https://github.com/srsgores
+	github: https://github.com/srsgores/
 
 	seng515-petroleum-explorer
 
@@ -9,57 +9,184 @@
 	=============================================================================
 	This file is responsible to generate the default chart (oil production).
 -------------------------------------------------------------------------------*/
+
  (function () {
 
-	 var index = 0;
-	 var minimums = [1,2,3,4,5,6,7];
-	 var maximums = [10, 20, 30 ,40 ,50, 60, 70];
-	 var wellsList;
-	 var maxList;
-	 var minList;
-
 	 var InfoGraph;
+	 var index = 0;
+	 var dataSet;
+	 var currentWells;
 
 	 InfoGraph = (function(){
 		 function InfoGraph(){
-			/*wellsList = wells;
-			maxList = maxs;
-			minList = mins;*/
 		 }
 	 });
 
 	 var self = this;
 
-	 InfoGraph.prototype.getMinMaxValues = function(allWells) {
-		var attributesToConsider = ["PHIc", "PHIR", "Vshc", "Soc", "KRc", "KRav", "H"];
-		var allValues = [];
-
-		for(var i=0; i<allWells.length; i++)
-			for(var j=0; j<attributesToConsider.length; j++)
-				allValues.push(allWells[i][attributesToConsider[j]]);
-
-		allValues.sort();
-
-		return [allValues[0], allValues[allValues.length - 1]];
-	 }
-
-	 InfoGraph.prototype.getMinMaxValuesFromAttribute = function(wells, attribute) {
-		var values = [];
-		for(var i=0; i<wells.length; i++)
-			values.push(wells[i][attribute]);
-		values.sort();
-
-		return [values[0], values[values.length - 1]];
-	 }
-
-	 // Normalize values that Shawn wrote.
-
-	 /*
-	  * TODO: Add comment!
-	  */
-	 InfoGraph.prototype.generateChart = function () {
+	 InfoGraph.prototype.generateChart = function(currentUWI, allCurrentWells, allDataSet) {
 
 		 $(function() {
+			 var currentIndex = [];
+			 dataSet = allDataSet;
+			 currentWells = allCurrentWells;
+
+			 for(var i=0; i<dataSet.length; i++)
+			 {
+				 if(dataSet[i]["Well_Unique_Identifier"] === currentUWI)
+				 {
+					 currentIndex.push(i);
+					 break;
+				 }
+			 }
+
+			 var minMax = getMinMaxValuesFromAllAttributes(dataSet);
+			 var minMaxValues =
+				 [
+					 [minMax[0],minMax[1]],
+					 [minMax[0],minMax[1]],
+					 [minMax[0],minMax[1]],
+					 [minMax[0],minMax[1]],
+					 [minMax[0],minMax[1]],
+					 [minMax[0],minMax[1]],
+					 [minMax[0],minMax[1]]
+				 ];
+
+			 /*
+			  * Values from all the wells
+			  */
+			 var PHIcAllValues = getValuesFromProperty(dataSet, "PHIc");
+			 var PHIRAllValues = getValuesFromProperty(dataSet, "PHIR");
+			 var VshcAllValues = getValuesFromProperty(dataSet, "Vshc");
+			 var SocAllValues = getValuesFromProperty(dataSet, "Soc");
+			 var KRcAllValues = getValuesFromProperty(dataSet, "KRc");
+			 var KRavAllValues = getValuesFromProperty(dataSet, "KRav");
+			 var HAllValues = getValuesFromProperty(dataSet, "H");
+
+			 /*
+			  * Values from the wells being shown on the map
+			  */
+			 var PHIcValues = getValuesFromProperty(currentWells, "PHIc");
+			 var PHIRValues = getValuesFromProperty(currentWells, "PHIR");
+			 var VshcValues = getValuesFromProperty(currentWells, "Vshc");
+			 var SocValues = getValuesFromProperty(currentWells, "Soc");
+			 var KRcValues = getValuesFromProperty(currentWells, "KRc");
+			 var KRavValues = getValuesFromProperty(currentWells, "KRav");
+			 var HValues = getValuesFromProperty(currentWells, "H");
+
+			 /*
+			  * Values from the current selected well
+			  */
+			 var PHIcCurrentSelectedWellValue = useDefinedNumberOfDecimalPlaces([PHIcAllValues[currentIndex]], 1)[0];
+			 var PHIRCurrentSelectedWellValue = useDefinedNumberOfDecimalPlaces([PHIRAllValues[currentIndex]], 1)[0];
+			 var VshcCurrentSelectedWellValue = useDefinedNumberOfDecimalPlaces([VshcAllValues[currentIndex]], 1)[0];
+			 var SocCurrentSelectedWellValue = useDefinedNumberOfDecimalPlaces([SocAllValues[currentIndex]], 1)[0];
+			 var KRcCurrentSelectedWellValue = useDefinedNumberOfDecimalPlaces([KRcAllValues[currentIndex]], 1)[0];
+			 var KRavCurrentSelectedWellValue = useDefinedNumberOfDecimalPlaces([KRavAllValues[currentIndex]], 1)[0];
+			 var HCurrentSelectedWellValue = useDefinedNumberOfDecimalPlaces([HAllValues[currentIndex]], 1)[0];
+
+			 var currentSelectedWellValues =
+				 [{
+					 "PHIc": PHIcCurrentSelectedWellValue,
+					 "PHIR": PHIRCurrentSelectedWellValue,
+					 "Vshc": VshcCurrentSelectedWellValue,
+					 "Soc": SocCurrentSelectedWellValue,
+					 "KRc": KRcCurrentSelectedWellValue,
+					 "KRav": KRavCurrentSelectedWellValue,
+					 "H": HCurrentSelectedWellValue
+				 }];
+
+			 /*
+			  * Indices of the wells being shown on the map in the 'all wells' set
+			  */
+			 var PHIcIndices = getValuesIndices(PHIcAllValues, PHIcValues);
+			 var PHIRIndices = getValuesIndices(PHIRAllValues, PHIRValues);
+			 var VshcIndices = getValuesIndices(VshcAllValues, VshcValues);
+			 var SocIndices = getValuesIndices(SocAllValues, SocValues);
+			 var KRcIndices = getValuesIndices(KRcAllValues, KRcValues);
+			 var KRavIndices = getValuesIndices(KRavAllValues, KRavValues);
+			 var HIndices = getValuesIndices(HAllValues, HValues);
+
+			 /*
+			  * Normalized value of all wells
+			  */
+			 var normalizedPHIcAllValues = normalize_data(PHIcAllValues, minMax[0], minMax[1]);
+			 var normalizedPHIRAllValues = normalize_data(PHIRAllValues, minMax[0], minMax[1]);
+			 var normalizedVshcAllValues = normalize_data(VshcAllValues, minMax[0], minMax[1]);
+			 var normalizedSocAllValues = normalize_data(SocAllValues, minMax[0], minMax[1]);
+			 var normalizedKRcAllValues = normalize_data(KRcAllValues, minMax[0], minMax[1]);
+			 var normalizedKRavAllValues = normalize_data(KRavAllValues, minMax[0], minMax[1]);
+			 var normalizedHAllValues = normalize_data(HAllValues, minMax[0], minMax[1]);
+
+			 /*
+			  * Normalized values of wells being shown on the map
+			  */
+			 var normalizedPHIcValues = getValuesFromIndices(normalizedPHIcAllValues, PHIcIndices);
+			 var normalizedPHIRValues = getValuesFromIndices(normalizedPHIRAllValues, PHIRIndices);
+			 var normalizedVshcValues = getValuesFromIndices(normalizedVshcAllValues, VshcIndices);
+			 var normalizedSocValues = getValuesFromIndices(normalizedSocAllValues, SocIndices);
+			 var normalizedKRcValues = getValuesFromIndices(normalizedKRcAllValues, KRcIndices);
+			 var normalizedKRavValues = getValuesFromIndices(normalizedKRavAllValues, KRavIndices);
+			 var normalizedHValues = getValuesFromIndices(normalizedHAllValues, HIndices);
+
+			 /*
+			  * Normalized values of the current selected well
+			  */
+			 var normalizedCurrentPHIcValue = getValuesFromIndices(normalizedPHIcAllValues, currentIndex);
+			 var normalizedCurrentPHIRValue = getValuesFromIndices(normalizedPHIRAllValues, currentIndex);
+			 var normalizedCurrentVshcValue = getValuesFromIndices(normalizedVshcAllValues, currentIndex);
+			 var normalizedCurrentSocValue = getValuesFromIndices(normalizedSocAllValues, currentIndex);
+			 var normalizedCurrentKRcValue = getValuesFromIndices(normalizedKRcAllValues, currentIndex);
+			 var normalizedCurrentKRavValue = getValuesFromIndices(normalizedKRavAllValues, currentIndex);
+			 var normalizedCurrentHValue = getValuesFromIndices(normalizedHAllValues, currentIndex);
+
+			 /*
+			  * Set with the normalized values of the current selected well
+			  */
+			 var normalizedCurrentValues =
+				 [
+					 useDefinedNumberOfDecimalPlaces(normalizedCurrentPHIcValue, 1)[0],
+					 useDefinedNumberOfDecimalPlaces(normalizedCurrentPHIRValue, 1)[0],
+					 useDefinedNumberOfDecimalPlaces(normalizedCurrentVshcValue, 1)[0],
+					 useDefinedNumberOfDecimalPlaces(normalizedCurrentSocValue, 1)[0],
+					 useDefinedNumberOfDecimalPlaces(normalizedCurrentKRcValue, 1)[0],
+					 useDefinedNumberOfDecimalPlaces(normalizedCurrentKRavValue, 1)[0],
+					 useDefinedNumberOfDecimalPlaces(normalizedCurrentHValue, 1)[0]
+				 ];
+
+			 var minMaxNormalizedData =
+				 [
+					 getMinMaxValues(normalizedPHIcValues),
+					 getMinMaxValues(normalizedPHIRValues),
+					 getMinMaxValues(normalizedVshcValues),
+					 getMinMaxValues(normalizedSocValues),
+					 getMinMaxValues(normalizedKRcValues),
+					 getMinMaxValues(normalizedKRavValues),
+					 getMinMaxValues(normalizedHValues)
+				 ];
+
+			 var minMaxActualData =
+				 [
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(PHIcValues), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(PHIRValues), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(VshcValues), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(SocValues), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(KRcValues), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(KRavValues), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(HValues), 1)
+				 ];
+
+			 var minMaxTotalData =
+				 [
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(getValuesFromProperty(dataSet, "PHIc")), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(getValuesFromProperty(dataSet, "PHIR")), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(getValuesFromProperty(dataSet, "Vshc")), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(getValuesFromProperty(dataSet, "Soc")), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(getValuesFromProperty(dataSet, "KRc")), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(getValuesFromProperty(dataSet, "KRav")), 1),
+					 useDefinedNumberOfDecimalPlaces(getMinMaxValues(getValuesFromProperty(dataSet, "H")), 1)
+				 ];
+
 			 $('#highchart-basic').highcharts({
 
 				 chart: {
@@ -68,7 +195,7 @@
 				 },
 
 				 title: {
-					 text: undefined//'Temperature variation by day'
+					 text: undefined
 				 },
 
 				 xAxis: {
@@ -97,7 +224,7 @@
 									 var paddingLow = (this.point.plotLow - this.point.plotHigh) - 18;
 
 									 html = '<div style="color:black;height:' + (this.point.plotHigh - this.point.plotLow) + 'px;padding:' + paddingLow + 'px 0px 0px 0px">';
-									 html += this.point.low;
+									 html += minMaxActualData[this.point.x][0];
 									 html += '</div>';
 									 return html;
 
@@ -105,11 +232,9 @@
 									 var paddingHigh = (this.point.plotHigh - this.point.plotLow);
 
 									 html = '<div style="color:black;height:' + (this.point.plotLow - this.point.plotHigh) + 'px;padding: '+ paddingHigh +'px 0px 0px 0px">';
-									 html += this.point.high;
+									 html += minMaxActualData[this.point.x][1];
 									 html += '</div>';
 									 return html;
-
-									 return this.point.high;
 								 }
 							 }
 						 }
@@ -117,18 +242,20 @@
 					 arearange: {
 						 dataLabels: {
 							 formatter: function() {
-
-								 // PUT THE RIGHT NUMBERS FOR THE MAXIMUM AND MINIMUM!
 								 if(this.y === this.point.low) {
-									 console.log("Low: " + index % 7);
-									 return minimums[index++ % 7];
+									 return minMaxTotalData[index++ % 7][0];
 								 }
 								 else {
-									 console.log("High: " + index % 7);
-									 return maximums[index++ % 7];
+									 return minMaxTotalData[index++ % 7][1];
 								 }
-
-
+							 }
+						 }
+					 },
+					 scatter: {
+						 dataLabels: {
+							 formatter: function() {
+								 // The scatter should not have labels to indicate values!
+								 return '';
 							 }
 						 }
 					 },
@@ -144,7 +271,23 @@
 
 				 tooltip: {
 					 formatter: function() {
-						 return false;
+						 if(this.series != undefined)
+						 {
+							 if(this.series.name === "actualValue")
+							 {
+								 return "<b>" + this.key + " current value</b><br>" + currentSelectedWellValues[0][this.key];
+							 }
+							 else if(this.series.name === "wellAttributes")
+							 {
+								 var content = "<b>" + this.key + " range</b><br>"
+									 + minMaxActualData[this.point.x][0] + " - " + minMaxActualData[this.point.x][1] + "<br>"
+									 + "<b>Current value</b><br>" + currentSelectedWellValues[0][this.key];
+								 return content;
+							 }
+							 else
+							 // to disable the tooltip at a point return false
+								 return false;
+						 }
 					 }
 				 },
 
@@ -154,34 +297,164 @@
 
 				 series: [
 					 {
-						 name: 'MinMaxValues',
-						 data: [[-10,35],[-10,35],[-10,35],[-10,35],[-10,35],[-10,35],[-10,35]],
-						 color: "#F0F0F0"
+						 name: 'minMaxValues',
+						 data: minMaxValues,
+						 color: "#D0D0D0",
+						 states: {
+							 hover: false
+						 },
+						 zIndex: 1
 					 },
 					 {
 						 type: 'columnrange',
 						 // We want a vertical chart
 						 inverted: false,
 
-						 name: 'WellAttributes',
-						 // The important part here would be grabing the max and min values from the wells
-						 data: [
-							 [-9.7, 9.4],
-							 [-8.7, 6.5],
-							 [-3.5, 9.4],
-							 [-1.4, 19.9],
-							 [0.0, 22.6],
-							 [2.9, 29.5],
-							 [9.2, 30.7]
-						 ]
+						 name: 'wellAttributes',
+						 // The important part here would be grabbing the max and min values from the wells
+						 data: minMaxNormalizedData,
+						 states: {
+							 hover: false
+						 },
+						 zIndex: 2
+					 },
+					 {
+						 type: 'scatter',
+						 name: 'actualValue',
+						 data: normalizedCurrentValues,
+						 marker: {
+							 fillColor: 'red',
+							 lineWidth: 2
+						 },
+						 zIndex: 3
 					 }
 				 ]
-
 			 });
 		 });
-	 }
-	 generateChart();
+	 };
 
+	 function getMinMaxValues(allWells) {
+		 var attributesToConsider = ["PHIc", "PHIR", "Vshc", "Soc", "KRc", "KRav", "H"];
+		 var allValues = [];
+
+		 for(var i=0; i<allWells.length; i++)
+			 for(var j=0; j<attributesToConsider.length; j++)
+				 allValues.push(allWells[i][attributesToConsider[j]]);
+
+		 allValues.sort();
+
+		 return [allValues[0], allValues[allValues.length - 1]];
+	 }
+
+	 function getMinMaxValuesFromAttribute(wells, attribute) {
+		 var values = [];
+		 for(var i=0; i<wells.length; i++)
+			 values.push(wells[i][attribute]);
+		 values.sort();
+
+		 return [values[0], values[values.length - 1]];
+	 }
+
+	 function getMinMaxValuesFromAllAttributes(allWells) {
+		 var attributesToConsider = ["PHIc", "PHIR", "Vshc", "Soc", "KRc", "KRav", "H"];
+
+		 var max = -999999999, min = 999999999;
+
+		 for(var i=0; i<allWells.length; i++)
+		 {
+			 for(var j=0; j<attributesToConsider.length; j++)
+			 {
+				 if(allWells[i][attributesToConsider[j]] > max)
+					 max = allWells[i][attributesToConsider[j]];
+				 if(allWells[i][attributesToConsider[j]] < min)
+					 min = allWells[i][attributesToConsider[j]];
+			 }
+		 }
+
+		 return [min, max];
+	 }
+
+	 function getMinMaxValues(values) {
+		 var max = -999999999, min = 999999999;
+		 for(var i=0; i<values.length; i++)
+		 {
+			 if(values[i] > max)
+				 max = values[i];
+			 if(values[i] < min)
+				 min = values[i];
+		 }
+
+		 return [min, max];
+	 }
+
+	 function getValuesFromProperty(wells, attribute) {
+		 var result = [];
+
+		 for(var i=0; i<wells.length; i++)
+			 result.push(wells[i][attribute]);
+
+		 return result;
+	 }
+
+	 function useDefinedNumberOfDecimalPlaces(values, decimalPlaces) {
+		 if(decimalPlaces < 0)
+			 return null;
+
+		 var result = [];
+
+		 for(var i=0; i<values.length; i++)
+			 result.push(parseFloat(values[i].toFixed(decimalPlaces)));
+
+		 return result;
+	 }
+
+	 /*
+	  * TODO: USE SHAWN'S NORMALIZATION TO DO IT!
+	  */
+	 function normalize_data (values, lowVal, highVal) {
+		 var N = values.length;
+
+		 //Get the minimum and maximum values:
+		 var minMax = getMinMaxValues(values);
+		 var min = minMax[0];
+		 var max = minMax[1];
+		 var range = max - min;
+		 var range2 = highVal - lowVal;
+
+		 var data_out = [];
+		 //If min=max, then return an array with all values set to (low_val + high_val)/2
+		 if (min === max)
+			 for (var i = 0; i < N; i++)
+				 data_out[i] = (lowVal + highVal) / 2;
+		 else //If min != max, then the data can be scaled by the factor (high_val-low_val)/(max-min) without dividing by 0.
+			 for (var i = 0; i < N; i++)
+			 {
+				 data_out[i] = (values[i] - min) / range;
+				 data_out[i] = (data_out[i] * range2) + lowVal;
+			 }
+
+		 return data_out;
+	 }
+
+	 function getValuesIndices(totalSet, subSet) {
+		 var indices = [];
+
+		 for(var i=0; i<subSet.length; i++)
+		 {
+			 indices.push(totalSet.indexOf(subSet[i]));
+		 }
+
+		 return indices;
+	 }
+
+	 function getValuesFromIndices(totalSet, indices) {
+		 var result = [];
+
+		 for(var i=0; i<indices.length; i++)
+			 result.push(totalSet[indices[i]]);
+
+		 return result;
+	 }
 
 	(typeof exports !== "undefined" && exports !== null ? exports : window).InfoGraph = InfoGraph;
  }).call(this);
