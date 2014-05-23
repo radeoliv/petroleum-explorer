704c80bbec5459c284774a75ad3ca380e7906ca5
Rodrigo Silva <radeoliv@ucalgary.ca>
2014-05-23 13:36:09 -0600

    Finished implementation of the bar chart. Still need to review the click event to consider other places than just the bars.

--- app/scripts/components/visualization/visualization-charts.js
+++ app/scripts/components/visualization/visualization-charts.js
@@ -38,12 +38,15 @@
 	VisualizationCharts.prototype.generateBarChart = function(attribute, attributeText) {
 		// Calculate the data distribution
 		var data = this.calculateBarChartValues(attribute);
+		var currentWells = this.MapController.getCurrentWells();
 
 		if(data != null && data.length > 0) {
 
 			var width = 800,
 				height = 600,
 				radius = Math.min(width, height)/1.5;
+
+			var color = d3.scale.category20();
 
 			var x = d3.scale.ordinal()
 				.rangeRoundBands([0, width], 0.1);
@@ -70,6 +73,8 @@
 				.attr("height", "98%")//height + margin.top + margin.bottom)
 				.attr("viewBox", "0 0 " + (2*radius+width/2) + " " + (2*radius))
 				.append("g")
+				.attr("width", (width + (width/3)))
+				.attr("height", height)
 				.attr("transform", "translate(" + (radius*0.2) + "," + (radius*0.4) + ")");
 
 
@@ -86,7 +91,7 @@
 					d = +d;
 				});
 
-				var count = 1;
+				var count = 0;
 				x.domain(data.map(function(d) { return count++; }));//d.letter; }));
 				y.domain([0, d3.max(data, function(d) { return d; })]);//d.frequency; })]);
 
@@ -101,20 +106,155 @@
 					.append("text")
 					.attr("transform", "rotate(-90)")
 					.attr("y", 6)
-					//.attr("dy", ".71em")
-					.attr("dy", "-4em")
-					.style("text-anchor", "end");
-					//.text(attributeText);
+					.attr("dy", ".71em")
+					//.attr("dy", "-4em")
+					.style({
+						"text-anchor": "end",
+						"font-size": "1.1em"
+					})
+					.text(attributeText);
 
-				count = 1;
+				var applyAllTextLegend = function(barId) {
+					// We have to append multiple SVGs since we can't use line break to do it
+					var index = 0;
+					applyTextLegend(barId, index++);
+					applyTextLegend(barId, index++);
+
+					var temp = index++;
+					svg.append("line")
+						.attr("id", "line-separator")
+						.attr("x1", width)
+						.attr("x2", (width + width/2.8))
+						// y function is pretty much the same as the text in applyTextLegend function
+						.attr("y1", (20 + (17 * temp)))
+						.attr("y2", (20 + (17 * temp)))
+						// The style is pretty much the same as the rectangle for the legends
+						.style({
+							"stroke": "black",
+							"stroke-width": "0.05em"
+						});
+
+					// UWI
+					applyTextLegend(barId, index++);
+					applyTextLegend(barId, index++);
+
+					// Company name
+					index++;
+					applyTextLegend(barId, index++);
+					applyTextLegend(barId, index++);
+
+					// Status
+					index++;
+					applyTextLegend(barId, index++);
+					applyTextLegend(barId, index++);
+				};
+
+				var applyTextLegend = function(barId, index) {
+					var getTextToAppend = function(barId, index) {
+						// The indices of the two values of the legend have to be x % 3 == 0 and x % 3 == 1
+						// This produces the line break
+						switch(index) {
+							case 0:
+								return attributeText;
+							case 1:
+								return currentWells[barId][attribute];
+
+							case 3:
+								return "UWI";
+							case 4:
+								return currentWells[barId]["Well_Unique_Identifier"];
+
+							case 6:
+								return "Well Operator"
+							case 7:
+								return currentWells[barId]["Well_Operator"];
+
+							case 9:
+								return "Well Status";
+							case 10:
+								return currentWells[barId]["Well_Status"];
+
+							default:
+								return "";
+						}
+					};
+
+					svg.append("text")
+						.attr("id", "text-info"+index)
+						.attr("class", "text-info")
+						.attr("x", (width + 10))
+						.attr("y", function(d) {
+							// Used to create spaces between the elements
+							return 20 + (20*index);
+						})
+						.style({
+							"font-size": function() { return index%3 === 0 ? "0.9em" : "0.8em" },
+							"font-weight": function() { return index%3 === 0 ? "bold" : "normal" }
+						})
+						.text(getTextToAppend(barId, index));
+				};
+
+				count = 0;
+				var count2 = 0;
+				var fixLegendContent = false;
 				svg.selectAll(".bar")
 					.data(data)
 					.enter().append("rect")
 					.attr("class", "bar")
-					.attr("x", function(d) { return x(count++); })//d.letter); })
+					.attr("id", function(d) { return "bar-" + (count++); })
+					.attr("x", function(d) { return x(count2++); })
+					.attr("y", function(d) { return y(d); })
 					.attr("width", x.rangeBand())
-					.attr("y", function(d) { return y(d); })//d.frequency); })
-					.attr("height", function(d) { return height - y(d); });//d.frequency); });
+					.on("mouseover", function() {
+						if(fixLegendContent === false) {
+							// Getting the bar id
+							var barId = $(this)[0].id;
+							// Highlight the bar
+							d3.select("#"+barId).style("fill", "darkorange");
+							// Removing all the characters that are not digits
+							barId = barId.replace(/^\D*/g, '');
+							// Inserting all legends
+							applyAllTextLegend(barId);
+						}
+					})
+					.on("mouseout", function() {
+						if(fixLegendContent === false) {
+							// Setting the normal color
+							d3.select("#" + $(this)[0].id).style("fill", "steelblue");
+							// Removing all text information
+							d3.selectAll(".text-info").remove();
+							d3.selectAll("#line-separator").remove();
+						}
+					})
+					.on("click", function() {
+						// The click event fix the information in the rectangle (legends)
+						fixLegendContent = !fixLegendContent;
+						if(fixLegendContent === false) {
+							d3.selectAll(".bar").style("fill", "steelblue");
+							d3.selectAll(".text-info").remove();
+							d3.selectAll("#line-separator").remove();
+						} else {
+							// Highlight the bar with another color
+							d3.select("#"+$(this)[0].id).style("fill", "orangered");
+						}
+					})
+					.transition().delay(function (d,i){ return i * 15;}).duration(200)
+					.attr("height", function(d) { return height - y(d); })
+					.style("fill", "steelblue");
+
+
+
+				var rect = svg.append("rect")
+					.attr("x", width)
+					.attr("y", 0)
+					.attr("width", (width / 2.8))
+					.attr("height", (height/2.5))
+					.style({
+						//"fill": "rgb(220,220,220)",
+						"fill": "none",
+						"stroke": "black",
+						"stroke-width": "0.05em"
+					});
 			}
 			drawD3Document(data);
 		}
@@ -213,7 +353,6 @@
 					.enter().append("g")
 					.attr("class", "arc")
 					.on("click", function() {
-						console.log($(this));
 						var arcId = $(this)[0].children[0].id;
 						arcId = arcId.replace(/^\D*/g, '');
 						var legend = d3.select("#legend-" + arcId);
