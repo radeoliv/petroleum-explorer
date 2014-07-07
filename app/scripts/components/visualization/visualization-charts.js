69a43c4b2596a2826327b9e4bcf25baadf8ab440
Rodrigo Silva <radeoliv@ucalgary.ca>
2014-07-07 15:25:20 -0600

    Added rickshaw plugin.

--- app/scripts/components/visualization/visualization-charts.js
+++ app/scripts/components/visualization/visualization-charts.js
@@ -911,103 +911,20 @@
 		this.removeCurrentChart();
 		$('<div id=\"canvas-svg\"></div>').appendTo($visualizationContainer);
 
-		console.log("WORKED!");
+		var graph = new Rickshaw.Graph( {
+			element: document.querySelector('#canvas-svg'),
+			series: [
+				{
+					color: 'steelblue',
+					data: [ { x: 0, y: 23}, { x: 1, y: 15 }, { x: 2, y: 79 } ]
+				}, {
+					color: 'lightblue',
+					data: [ { x: 0, y: 30}, { x: 1, y: 20 }, { x: 2, y: 64 } ]
+				}
+			]
+		} );
 
-		var margin = {top: 20, right: 20, bottom: 30, left: 40},
-			width = 960 - margin.left - margin.right,
-			height = 500 - margin.top - margin.bottom;
-
-		/*var x0 = d3.scale.ordinal()
-			.rangeRoundBands([0, width], .1);
-		*/
-
-		var x0 = d3.time.scale()
-			.range([0, width]);
-
-		var x1 = d3.scale.ordinal();
-
-		var y = d3.scale.linear()
-			.range([height, 0]);
-
-		var color = d3.scale.ordinal()
-			.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-		var xAxis = d3.svg.axis()
-			.scale(x0)
-			.orient("bottom");
-
-		var yAxis = d3.svg.axis()
-			.scale(y)
-			.orient("left")
-			.tickFormat(d3.format(".2s"));
-
-		var svg = d3.select("#canvas-svg").append("svg")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom)
-			.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-		d3.csv("sp500.csv", function(error, data) {
-			var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
-
-			data.forEach(function(d) {
-				d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
-			});
-
-			x0.domain(data.map(function(d) { return d.State; }));
-			x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
-			y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
-
-			svg.append("g")
-				.attr("class", "x axis")
-				.attr("transform", "translate(0," + height + ")")
-				.call(xAxis);
-
-			svg.append("g")
-				.attr("class", "y axis")
-				.call(yAxis)
-				.append("text")
-				.attr("transform", "rotate(-90)")
-				.attr("y", 6)
-				.attr("dy", ".71em")
-				.style("text-anchor", "end")
-				.text("Population");
-
-			var state = svg.selectAll(".state")
-				.data(data)
-				.enter().append("g")
-				.attr("class", "g")
-				.attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
-
-			state.selectAll("rect")
-				.data(function(d) { return d.ages; })
-				.enter().append("rect")
-				.attr("width", x1.rangeBand())
-				.attr("x", function(d) { return x1(d.name); })
-				.attr("y", function(d) { return y(d.value); })
-				.attr("height", function(d) { return height - y(d.value); })
-				.style("fill", function(d) { return color(d.name); });
-
-			var legend = svg.selectAll(".legend")
-				.data(ageNames.slice().reverse())
-				.enter().append("g")
-				.attr("class", "legend")
-				.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-			legend.append("rect")
-				.attr("x", width - 18)
-				.attr("width", 18)
-				.attr("height", 18)
-				.style("fill", color);
-
-			legend.append("text")
-				.attr("x", width - 24)
-				.attr("y", 9)
-				.attr("dy", ".35em")
-				.style("text-anchor", "end")
-				.text(function(d) { return d; });
-
-		});
+		graph.render();
 	}
 
 	VisualizationCharts.prototype.removeCurrentChart = function() {
