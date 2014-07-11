226b02d84e780875c2b954b028b05476c2c0b4f1
Rodrigo Silva <radeoliv@ucalgary.ca>
2014-07-11 11:51:22 -0600

    Updated the ease (transition) of bar chart.

--- app/scripts/components/visualization/visualization-charts.js
+++ app/scripts/components/visualization/visualization-charts.js
@@ -257,7 +257,7 @@
 					})
 					.attr("height", 0)
 					.attr("y", height)
-					.transition().duration(1000).ease("bounce-in")
+					.transition().duration(1600).ease("elastic-in")
 					.attr("y", function(d) { return y(d); })
 					.attr("height", function(d) { return height - y(d); })
 					.style("fill", function(d,i) {
@@ -501,130 +501,6 @@
 					});
 			};
 			drawD3Document(data);
-		}
-	};
-
-	VisualizationCharts.prototype.generateTimeSeriesChart = function(attribute) {
-		var parseDate = d3.time.format("%b %Y").parse;
-
-		/*var dates = ["Jan 2000","Feb 2000","Mar 2000","Apr 2000","May 2000","Jun 2000","Jul 2000","Aug 2000",//"Sep 2000","Oct 2000","Nov 2000","Dec 2000",
-		//"Jan 2001","Feb 2001","Mar 2001","Apr 2001","May 2001","Jun 2001","Jul 2001",
-			"Aug 2001","Sep 2001","Oct 2001","Nov 2001","Dec 2001"];*/
-		var dates = ["Dec 2005", "Jan 2006", "May 2006"];
-		var values = [1,2,3,4,5,6,7,8,9,10,11,12,11,10,9,8,7,6,5,4,3,2,1,0];
-
-		var dataTest = [];
-		for(var i=0; i<dates.length; i++) {
-			dataTest.push({"date": parseDate(dates[i]), "price":values[i]});
-		}
-
-		/* TESTING STUFF */
-		this.removeCurrentChart();
-		$('<div id=\"canvas-svg\"></div>').appendTo($visualizationContainer);
-		/* TESTING STUFF */
-		/* Testing stuff ^^^^^^^^ */
-
-		var margin = {top: 10, right: 10, bottom: 100, left: 40},
-			margin2 = {top: 430, right: 10, bottom: 20, left: 40},
-			width = 960 - margin.left - margin.right,
-			height = 500 - margin.top - margin.bottom,
-			height2 = 500 - margin2.top - margin2.bottom;
-
-		var x = d3.time.scale().range([0, width]),
-			x2 = d3.time.scale().range([0, width]),
-			y = d3.scale.linear().range([height, 0]),
-			y2 = d3.scale.linear().range([height2, 0]);
-
-		var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-			xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
-			yAxis = d3.svg.axis().scale(y).orient("left");
-
-		var brush = d3.svg.brush()
-			.x(x2)
-			.on("brush", brushed);
-
-		var area = d3.svg.area()
-			//.interpolate("monotone")
-			.interpolate("cardinal").tension(0.94)
-			//.interpolate("linear")
-			.x(function(d) { return x(d.date); })
-			.y0(height)
-			.y1(function(d) { return y(d.price); });
-
-		var area2 = d3.svg.area()
-			//.interpolate("monotone")
-			.interpolate("cardinal").tension(0.94)
-			//.interpolate("linear")
-			.x(function(d) { return x2(d.date); })
-			.y0(height2)
-			.y1(function(d) { return y2(d.price); });
-
-		var svg = d3.select("#canvas-svg").append("svg")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom);
-
-		svg.append("defs").append("clipPath")
-			.attr("id", "clip")
-			.append("rect")
-			.attr("width", width)
-			.attr("height", height);
-
-		var focus = svg.append("g")
-			.attr("class", "focus")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-		var context = svg.append("g")
-			.attr("class", "context")
-			.attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-		//d3.csv("./sp500.csv", type, function(error, data) {
-			x.domain(d3.extent(dataTest.map(function(d) { return d.date; })));
-			y.domain([0, d3.max(dataTest.map(function(d) { return d.price; }))]);
-			x2.domain(x.domain());
-			y2.domain(y.domain());
-
-			focus.append("path")
-				.datum(dataTest)
-				.attr("class", "area")
-				.attr("d", area);
-
-			focus.append("g")
-				.attr("class", "x axis")
-				.attr("transform", "translate(0," + height + ")")
-				.call(xAxis);
-
-			focus.append("g")
-				.attr("class", "y axis")
-				.call(yAxis);
-
-			context.append("path")
-				.datum(dataTest)
-				.attr("class", "area")
-				.attr("d", area2);
-
-			context.append("g")
-				.attr("class", "x axis")
-				.attr("transform", "translate(0," + height2 + ")")
-				.call(xAxis2);
-
-			context.append("g")
-				.attr("class", "x brush")
-				.call(brush)
-				.selectAll("rect")
-				.attr("y", -6)
-				.attr("height", height2 + 7);
-		//});
-
-		function brushed() {
-			x.domain(brush.empty() ? x2.domain() : brush.extent());
-			focus.select(".area").attr("d", area);
-			focus.select(".x.axis").call(xAxis);
-		}
-
-		function type(d) {
-			d.date = parseDate(d.date);
-			d.price = +d.price;
-			return d;
 		}
 	};
 
